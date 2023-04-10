4ef7fa2c7ac438046abf824683aa73cf78ee1a19
Julian-IGS <julian.laspalles1@campus-igs-toulouse.fr>
2023-04-10 22:10:17 +0200

    avec du css un peu degeu

--- front/src/component/Modal.jsx
+++ front/src/component/Modal.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Modal() {
   const { pokedexId } = useParams();
@@ -12,21 +13,20 @@ export default function Modal() {
   }, [pokedexId]);
 
   return (
-    <div>
-      <img src={pokemon.picture} alt={pokemon.name} />
-      <div>Height: {pokemon.height}cm</div>
-      <div>Weight: {pokemon.weight}kg</div>
-      {/* <div>Stats:</div>
-      <ul>
-      <li>HP: {pokemon.stats.hp}</li>
-        <li>Attack: {pokemon.stats.attack}</li>
-        <li>Defense: {pokemon.stats.defense}</li>
-        <li>Special Attack: {pokemon.stats.special_attack}</li>
-        <li>Special Defense: {pokemon.stats.special_defense}</li>
-        <li>Speed: {pokemon.stats.speed}</li>
-      </ul> */}
-      <Link to="/">Retour au Pokedex !</Link>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card shadow-sm">
+            <img src={pokemon.picture} alt={pokemon.name} className="card-img-top" />
+            <div className="card-body">
+              <h5 className="card-title">{pokemon.name}</h5>
+              <p className="card-text">Height: {pokemon.height}cm</p>
+              <p className="card-text">Weight: {pokemon.weight}kg</p>
+              <Link to="/" className="btn btn-primary">Retour au Pokedex !</Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
   );
 }
--- front/src/component/Pokedex.jsx
+++ front/src/component/Pokedex.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Pokedex() {
   const [pokemons, setPokemons] = useState([]);
@@ -8,22 +9,26 @@ function Pokedex() {
     fetch('http://localhost:5000/api/pokemon')
       .then(response => response.json())
       .then(data => setPokemons(data))
-     
   }, []);
-// Creer une méthode pour afficher le pokemon selectionner
-// depuis onclick
 
   return (
-
-    <div>
-    {pokemons.map(pokemon => (
-      <div key={pokemon.pokedexId} >
-            <Link to={`/${pokemon._id}`}>{pokemon.name}</Link>
+    <div className="container">
+      <h3 className="text-center my-3">Liste des Pokemons</h3>
+      <div className="row">
+        {pokemons.map(pokemon => (
+          <div className="col-md-3 mb-3" key={pokemon.pokedexId}>
+            <div className="card">
+              <img src={pokemon.picture} alt={pokemon.name} className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">{pokemon.name}</h5>
+                <Link to={`/${pokemon._id}`} className="btn btn-primary">Voir détails</Link>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-    ))}
     </div>
   )
-
-  
 }
+
 export default Pokedex;
