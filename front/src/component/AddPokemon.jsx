c2cbf880c0d603f038ebffdb97be34c0ea09a1da
Julian-IGS <julian.laspalles1@campus-igs-toulouse.fr>
2023-04-25 22:01:48 +0200

    tentative de mettre joi
    pb il voit jamais hp ect...

--- front/src/component/AddPokemon.jsx
+++ front/src/component/AddPokemon.jsx
@@ -1,5 +1,7 @@
 import React, { useState} from 'react';
 import "../CSS/add.scss";
+import Joi from 'joi';
+
 
 function AddPokemon() {
     const [name, setName] = useState(''); // On utilise le hook useState pour initialiser l'état du nom du pokemon à une valeur vide
@@ -14,6 +16,20 @@ function AddPokemon() {
     const [speed, setSpeed] = useState(0);
 
 
+    const schema = Joi.object({
+      name: Joi.string().required(), // le nom et l'image sont des chaînes de caractères obligatoires
+      picture: Joi.string().uri().required(),
+      height: Joi.number().positive().required(),
+      weight: Joi.number().positive().required(),
+      hp: Joi.number().positive().required(),
+      attack: Joi.number().positive().required(), // nombres positifs obligatoires
+      defense: Joi.number().positive().required(),
+      special_attack: Joi.number().positive().required(),
+      special_defense: Joi.number().positive().required(),
+      speed: Joi.number().positive().required()
+    });
+
+
     const getPokemonId = async() => {
         const pokemons = await fetch('http://localhost:5000/api/pokemon') // Requête pour récupérer la liste des pokemons depuis l'API
         .then(response => response.json())
@@ -23,6 +39,7 @@ function AddPokemon() {
     const handleSubmit = async (event) => {
       event.preventDefault(); // Empêche la soumission du formulaire
       const pokedexId = await getPokemonId()
+    
   const data = 
     {
         pokedexId, 
@@ -41,6 +58,13 @@ function AddPokemon() {
         "elements": "63ce798adbde71362b88c3b1",
         "learningSkills": "63d0e0f1eaac93155b36a49d",
   }
+
+  const { error } = schema.validate(data); // Valide les données avec le schéma
+  if (error) {
+    console.log(error.details[0].message); // Affiche le message d'erreur dans la console
+    return;
+  }
+
   console.log(data)
       // Envoie une requête POST à l'API pour ajouter le nouveau pokemon
         fetch('http://localhost:5000/api/pokemon', {
@@ -59,99 +83,99 @@ function AddPokemon() {
         <form id="form" onSubmit={handleSubmit}>
         <p>Ajouter votre pokemon.</p>
      
-        <div class="form-group">
+        <div className="form-group">
            <label htmlFor="name">Nom</label>
                  <br />
-                 <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i>            </div>
+                 <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i>            </div>
               <input type="text" id="name" value={name} onChange={e => setName(e.target.value)} required />
               </div>
            </div>
            
-        <div class="form-group">
+        <div className="form-group">
            <label htmlFor="height">Hauteur</label>
                  <br />
-                 <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i>            </div>
-              <input type="text" id="height" value={height} onChange={e => setHeight(e.target.value)} required />
+                 <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i>            </div>
+              <input type="text" id="height" value={height} onChange={e => setHeight(parseInt(e.target.value))} required />
               </div>
            </div>
            
-           <div class="form-group">
+           <div className="form-group">
            <label htmlFor="weight">Poids</label>
                  <br />
-                 <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i>            </div>
-              <input type="text" id="weight" value={weight} onChange={e => setWeight(e.target.value)} required />
+                 <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i>            </div>
+              <input type="text" id="weight" value={weight} onChange={e => setWeight(parseInt(e.target.value))} required />
               </div>
            </div>
            
-           <div class="form-group">
-           <label htmlFor="HP">Vie</label>
+           <div className="form-group">
+           <label htmlFor="hp">Vie</label>
                  <br />
-                 <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i>            </div>
-              <input type="text" id="hp" value={hp} onChange={e => setHp(e.target.value)} required />
+                 <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i>            </div>
+              <input type="text" id="hp" value={hp} onChange={e => setHp(parseInt(e.target.value))} required />
               </div>
            </div>
            
-           <div class="form-group">
+           <div className="form-group">
            <label htmlFor="attack">Attaque</label>
                  <br />
-                 <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i>            </div>
-              <input type="text" id="attack" value={attack} onChange={e => setAttack(e.target.value)} required />
+                 <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i>            </div>
+              <input type="text" id="attack" value={attack} onChange={e => setAttack(parseInt(e.target.value))} required />
               </div>
            </div>
 
-           <div class="form-group">
+           <div className="form-group">
            <label htmlFor="defense">Defense</label>
                  <br />
-                 <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i>            </div>
-              <input type="text" id="defense" value={defense} onChange={e => setDefense(e.target.value)} required />
+                 <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i>            </div>
+              <input type="text" id="defense" value={defense} onChange={e => setDefense(parseInt(e.target.value))} required />
               </div>
            </div>
            
-           <div class="form-group">
+           <div className="form-group">
            <label htmlFor="special_attack">Attaque spéciale</label>
                  <br />
-                 <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i>            </div>
-              <input type="text" id="special_attack" value={special_attack} onChange={e => setSpecial_attack(e.target.value)} required />
+                 <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i>            </div>
+              <input type="text" id="special_attack" value={special_attack} onChange={e => setSpecial_attack(parseInt(e.target.value))} required />
               </div>
            </div>
            
-           <div class="form-group">
+           <div className="form-group">
            <label htmlFor="special_defense">Defense spéciale</label>
                  <br />
-                 <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i>            </div>
-              <input type="text" id="special_defense" value={special_defense} onChange={e => setSpecial_defense(e.target.value)} required />
+                 <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i>            </div>
+              <input type="text" id="special_defense" value={special_defense} onChange={e => setSpecial_defense(parseInt(e.target.value))} required />
               </div>
            </div>
            
-           <div class="form-group">
+           <div className="form-group">
            <label htmlFor="speed">Vitesse</label>
                  <br />
-                 <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i>            </div>
-              <input type="text" id="speed" value={speed} onChange={e => setSpeed(e.target.value)} required />
+                 <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i>            </div>
+              <input type="text" id="speed" value={speed} onChange={e => setSpeed(parseInt(e.target.value))} required />
               </div>
            </div>
-           <div class="form-group">
+           <div className="form-group">
            <label htmlFor="picture">Photo</label>
                  <br />
-                 <div class="input-group">
-              <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i>            </div>
+                 <div className="input-group">
+              <div className="input-group-addon"><i className="fa fa-user" aria-hidden="true"></i>            </div>
               <input type="text" id="picture" value={picture} onChange={e => setPicture(e.target.value)} required />
               </div>
            </div>
-        <button type="submit" class="btn btn-default">Sauvegarder</button>
+        <button type="submit" className="btn btn-default">Sauvegarder</button>
      </form>   
      
     );
   }
-  
+
   export default AddPokemon;
   
