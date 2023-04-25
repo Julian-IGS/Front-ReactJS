caf8cb4ee6dbb5377b67a0f37901dbddc735595b
Julian-IGS <julian.laspalles1@campus-igs-toulouse.fr>
2023-04-25 12:01:55 +0200

    finis formulaire avec champs

--- front/src/component/AddPokemon.jsx
+++ front/src/component/AddPokemon.jsx
@@ -5,35 +5,49 @@ function AddPokemon() {
     const [picture, setPicture] = useState(''); // On utilise le hook useState pour initialiser l'état de l'image du pokemon à une valeur vide
     const [height, setHeight] = useState(0);
     const [weight, setWeight] = useState(0);
-    
+    const [hp, setHp] = useState(0);
+    const [attack, setAttack] = useState(0);
+    const [defense, setDefense] = useState(0);
+    const [special_attack, setSpecial_attack] = useState(0);
+    const [special_defense, setSpecial_defense] = useState(0);
+    const [speed, setSpeed] = useState(0);
 
-    const handleSubmit = (event) => {
+    const getPokemonId = async() => {
+        const pokemons = await fetch('http://localhost:5000/api/pokemon') // Requête pour récupérer la liste des pokemons depuis l'API
+        .then(response => response.json())
+        return pokemons.length+1
+    }
+
+    const handleSubmit = async (event) => {
       event.preventDefault(); // Empêche la soumission du formulaire
-  
-      // Envoie une requête POST à l'API pour ajouter le nouveau pokemon
-      fetch('http://localhost:5000/api/pokemon', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+      const pokedexId = await getPokemonId()
+  const data = 
+    {
+        pokedexId, 
+        name,
+        height,
+        weight,
+        stats:{
+        hp,
+        attack,
+        defense,
+        special_attack,
+        special_defense,
+        speed,
         },
-        body: JSON.stringify({
-            "pokedexId": 500,
-            "name": "test",
-            "height": 200,
-            "weight": 18.7,
-            "stats": {
-              "hp": 30,
-              "attack": 40,
-              "defense": 30,
-              "special_attack": 40,
-              "special_defense": 30,
-              "speed": 40
+        picture,
+        "elements": "63ce798adbde71362b88c3b1",
+        "learningSkills": "63d0e0f1eaac93155b36a49d",
+  }
+  console.log(data)
+      // Envoie une requête POST à l'API pour ajouter le nouveau pokemon
+        fetch('http://localhost:5000/api/pokemon', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
             },
-            "picture": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-            "elements": "63ce798adbde71362b88c3b1",
-            "learningSkills": "63d0e0f1eaac93155b36a49d"
-          }),          
-      })
+            body: JSON.stringify(data),          
+        })
         .then(response => response.json())
         .then(data => console.log(data)) // Affiche les données renvoyées par l'API dans la console
         .catch(error => console.error(error)); // Affiche l'erreur dans la console s'il y en a une
@@ -56,39 +70,31 @@ function AddPokemon() {
           </div>
           <div className="form-group">
             <label htmlFor="name">hp</label>
-            <input type="text" className="form-control" id="name" value={name} onChange={e => setName(e.target.value)} required />
+            <input type="text" className="form-control" id="hp" value={hp} onChange={e => setHp(e.target.value)} required />
           </div>
           <div className="form-group">
             <label htmlFor="name">attack</label>
-            <input type="text" className="form-control" id="name" value={name} onChange={e => setName(e.target.value)} required />
+            <input type="text" className="form-control" id="attack" value={attack} onChange={e => setAttack(e.target.value)} required />
           </div>
           <div className="form-group">
             <label htmlFor="name">defense</label>
-            <input type="text" className="form-control" id="name" value={name} onChange={e => setName(e.target.value)} required />
+            <input type="text" className="form-control" id="defense" value={defense} onChange={e => setDefense(e.target.value)} required />
           </div>
           <div className="form-group">
             <label htmlFor="name">special_attack</label>
-            <input type="text" className="form-control" id="name" value={name} onChange={e => setName(e.target.value)} required />
+            <input type="text" className="form-control" id="special_attack" value={special_attack} onChange={e => setSpecial_attack(e.target.value)} required />
           </div>
           <div className="form-group">
             <label htmlFor="name">special_defense</label>
-            <input type="text" className="form-control" id="name" value={name} onChange={e => setName(e.target.value)} required />
+            <input type="text" className="form-control" id="special_defense" value={special_defense} onChange={e => setSpecial_defense(e.target.value)} required />
           </div>
           <div className="form-group">
             <label htmlFor="name">speed</label>
-            <input type="text" className="form-control" id="name" value={name} onChange={e => setName(e.target.value)} required />
+            <input type="text" className="form-control" id="speed" value={speed} onChange={e => setSpeed(e.target.value)} required />
           </div>
           <div className="form-group">
-            <label htmlFor="picture">Image</label>
+            <label htmlFor="picture">Picture</label>
             <input type="text" className="form-control" id="picture" value={picture} onChange={e => setPicture(e.target.value)} required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="name">elements</label>
-            <input type="text" className="form-control" id="name" value={name} onChange={e => setName(e.target.value)} required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="name">learningSkills</label>
-            <input type="text" className="form-control" id="name" value={name} onChange={e => setName(e.target.value)} required />
           </div>
           <button type="submit" className="btn">Ajouter</button>
         </form>
